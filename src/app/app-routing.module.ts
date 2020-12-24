379d81fcff00bf3f96b6457b895af76b289f9746
kiran328 <kiran.koravi@buzzybrains.com>
2020-12-24 12:38:35 +0530

    added dashboard screen on login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { HierarchyComponent } from './hierarchy/hierarchy.component';
+import { CustomerComponent } from './customer/customer.component';
 
 
 const routes: Routes = [
@@ -14,6 +16,14 @@ const routes: Routes = [
   {
     path: "login",
     component: LoginComponent,
+  },
+  {
+    path: "hierarchy",
+    component: HierarchyComponent
+  },
+  {
+    path: "customer",
+    component: CustomerComponent
   }
 ];
 
